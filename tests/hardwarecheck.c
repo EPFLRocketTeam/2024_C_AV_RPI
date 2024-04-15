3dbf6ff315a3850e4fdc69527eb7f4f598c2ac72
marinPh <marin.philippe@epfl.ch>
2024-04-15 15:19:52 +0200

    can we run ? but we caninstall can I change cpu info ? checking with we are working with

---
+++ tests/hardwarecheck.c
@@ -0,0 +1,44 @@
+//
+// Created by marin on 15/04/2024.
+//
+
+#include <cstdio>
+#include <strings.h>
+
+unsigned gpioHardwareRevision(void)
+{
+   static unsigned rev = 0;
+
+   FILE * filp;
+   char buf[512];
+   char term;
+
+   if (rev) return rev;
+
+   filp = fopen ("/proc/cpuinfo", "r");
+
+
+   if (filp != NULL)
+   {
+      while (fgets(buf, sizeof(buf), filp) != NULL)
+      {
+         if (!strncasecmp("revision\t:", buf, 10))
+         {
+            if (sscanf(buf+10, "%x%c", &rev, &term) == 2)
+            {
+               if (term != '\n') rev = 0;
+            }
+         }
+      }
+      fclose(filp);
+   }
+
+   printf("----> rev = %x\n", rev);
+
+
+
+   return rev;
+}
+
+
+/* ----------------------------------------------------------------------- */
