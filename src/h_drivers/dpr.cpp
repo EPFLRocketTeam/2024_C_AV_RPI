02ea630b52339a52d6f1bba2216b4661036099ed
theo.hollender <theo.hollender@gmail.com>
2025-03-08 12:38:29 +0100

    dpr-driver: added dpr driver

---
+++ src/h_drivers/dpr.cpp
@@ -0,0 +1,105 @@
+
+#include "dpr.h"
+#include "i2c_wrappers.h"
+#include <PacketDefinition_Firehorn.h>
+
+/*
+ * I2C Init / Close
+ */
+
+uint8_t DPR_Driver::init () {
+    uint8_t suc = i2c_open(DPR_ADDRESS);
+    if (suc != 0) return DPR_FAIL_I2C_OPEN;
+
+    suc = get_intf_ptr(DPR_ADDRESS, &this->intf_ptr);
+    if (suc != 0) return DPR_FAIL_I2C_OPEN;
+
+    return 0;
+}
+uint8_t DPR_Driver::close () {
+    if (this->intf_ptr == nullptr) return 0;
+
+    uint8_t suc = i2c_close( DPR_ADDRESS );
+    if (suc != 0) return DPR_FAIL_I2C_CLOSE;
+
+    this->intf_ptr = 0;
+    return 0;
+}
+
+/*
+ * I2C Read / Write
+ */
+
+uint8_t DPR_Driver::read_reg (uint8_t reg_addr, uint8_t* reg_data, uint32_t len) {
+    if (this->intf_ptr == nullptr) return DPR_FAIL_I2C_NOT_OPEN;
+
+    uint8_t suc = i2c_read( reg_addr, reg_data, len, this->intf_ptr );
+    if (suc != 0) return DPR_FAIL_I2C_READ;
+
+    return 0;
+}
+uint8_t DPR_Driver::write_reg (uint8_t reg_addr, uint8_t* reg_data, uint32_t len) {
+    if (this->intf_ptr == nullptr) return DPR_FAIL_I2C_NOT_OPEN;
+
+    uint8_t suc = i2c_write( reg_addr, reg_data, len, this->intf_ptr );
+    if (suc != 0) return DPR_FAIL_I2C_WRITE;
+
+    return 0;
+}
+
+void DPR_Driver::check_policy(Data::GoatReg reg, const DataDump& dump) {
+    switch (dump.av_state) {
+        case State::CALIBRATION:
+            wakeUp();
+            break;
+        case State::MANUAL:
+            processManualMode(dump);
+            break;
+        case State::ARMED:
+            if (dump.event.command_updated
+             && dump.telemetry_cmd.id == CMD_ID::AV_CMD_PRESSURIZE)
+                pressurize();
+
+            break ;
+        default:
+            break;
+    }
+}
+
+void DPR_Driver::processManualMode(const DataDump& dump) {
+    if (dump.event.command_updated) {
+        switch (dump.telemetry_cmd.id) {
+            case CMD_ID::AV_CMD_ABORT:
+                abort();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void DPR_Driver::wakeUp () {
+    uint8_t wakeUpBuffer[1] = { 42 };
+    write_reg( (uint8_t) DPR_Commands::WAKE_UP, wakeUpBuffer, 1 );
+}
+void DPR_Driver::pressurize () {
+    uint8_t pressurizeBuffer[1] = { 42 };
+    write_reg( (uint8_t) DPR_Commands::PRESSURIZE, pressurizeBuffer, 1 );
+}
+void DPR_Driver::abort () {
+    uint8_t abortBuffer[1] = { 42 };
+    write_reg( (uint8_t) DPR_Commands::ABORT, abortBuffer, 1 );
+}
+
+float DPR_Driver::read_pressure () {
+    float res;
+
+    read_reg( (uint8_t) DPR_Commands::PRESSURE, (uint8_t*) (&res), 4);
+    return res;
+}
+bool DPR_Driver::read_dn_nc () {
+    uint8_t res;
+    read_reg( (uint8_t) DPR_Commands::DN_NC, &res, 1 );
+
+    return res != 0;
+}
---
+++ src/h_drivers/dpr.h
@@ -0,0 +1,54 @@
+
+#ifndef DPR_DRIVER_H
+#define DPR_DRIVER_H
+
+#include "h_driver.h"
+
+// TODO change the DPR Address
+#define DPR_ADDRESS 0xFF
+
+#define DPR_FAIL_I2C_OPEN  -1
+#define DPR_FAIL_I2C_CLOSE -2
+#define DPR_FAIL_I2C_NOT_OPEN -3
+#define DPR_FAIL_I2C_READ -4
+#define DPR_FAIL_I2C_WRITE -5
+
+enum class DPR_Commands : uint8_t {
+    // WRITE
+    TIMESTAMP_MAIN_FSM,
+    WAKE_UP,
+    PRESSURIZE,
+    ABORT,
+
+    // READ
+    INTERNAL_FSM_STATUS,
+    PRESSURE,
+    DN_NC
+};
+
+class DPR_Driver : public HDriver {
+public:
+    DPR_Driver  () = default;
+    ~DPR_Driver () = default;
+
+    uint8_t init  ();
+    uint8_t close ();
+
+    void check_policy(Data::GoatReg reg, const DataDump& dump) override;
+
+    float read_pressure ();
+    bool  read_dn_nc ();
+private:
+    void *intf_ptr = nullptr;
+
+    uint8_t  read_reg (uint8_t reg_addr, uint8_t* reg_data, uint32_t len);
+    uint8_t write_reg (uint8_t reg_addr, uint8_t* reg_data, uint32_t len);
+
+    void processManualMode(const DataDump& dump);
+
+    void wakeUp ();
+    void abort  ();
+    void pressurize ();
+};
+
+#endif
