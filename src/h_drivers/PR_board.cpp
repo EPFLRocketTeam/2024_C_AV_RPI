218b8acc9714de4c55f4f3f7b894e8b2e812f3df
marinPh <marin.philippe@epfl.ch>
2025-03-08 12:18:25 +0100

    excluded post flight

--- src/h_drivers/PR_board.cpp
+++ src/h_drivers/PR_board.cpp
@@ -6,34 +6,11 @@
 // if adress missing put a fix me and set the adress to 0x00 in the meantime 
 
 // Enumeration for the command of the PR board
-enum class PRBCommand {
-    // Write Commands
-    TIMESTAMP_MAIN_FSM,
-    WAKE_UP,
-    CLEAR_TO_IGNITE,
-    // Read Commands
-    IS_WOKEN_UP,
-    FSM_PRB,
-    P_OIN,
-    T_OIN,
-    P_EIN,
-    T_EIN,
-    P_CCC,
-    T_CCC,
-    P_CIG,
-    T_CIG,
-    // Read/Write Command
-    STATES_VANNES
-};
+
 
 // Define constants for valve bit positions
 // TODO: Check whether the order is ok or not
-#define VE_NO  (1 << 5)  
-#define VO_NOC (1 << 4)
-#define ME_B   (1 << 3)
-#define MO_BC  (1 << 2)
-#define IE_NC  (1 << 1)
-#define IO_NCC (1 << 0)
+
 
 PR_board::PR_board() {
     // Initialize the board by setting all the valves to closed
