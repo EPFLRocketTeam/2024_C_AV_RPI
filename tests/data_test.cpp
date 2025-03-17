a73f25e0ad5473aec39f4cac096fc351ec16935c
clemfavre <clemm.favre@gmail.com>
2025-03-17 18:06:15 +0100

    added tests for raw navigation sensors data

--- tests/data_test.cpp
+++ tests/data_test.cpp
@@ -17,6 +17,29 @@ bool areEqualBmp3_status(const bmp3_status& s1, const bmp3_status& s2) {
     );
 }
 
+bool areEqualAdxl1375_data(const adxl375_data& s1, const adxl375_data& s2) {
+    return (
+        s1.x == s2.x &&
+        s1.y == s2.y &&
+        s1.z == s2.z
+    );
+}
+
+bool areEqualbmi08_sensor_data_f(const bmi08_sensor_data_f& s1, const bmi08_sensor_data_f& s2) {
+    return (
+        s1.x == s2.x &&
+        s1.y == s2.y &&
+        s1.z == s2.z
+    );
+}
+
+bool areEqualbmp3_data(const bmp3_data& s1, const bmp3_data& s2) {
+    return (
+        s1.pressure == s2.pressure &&
+        s1.temperature == s2.temperature
+    );
+}
+
 int main(int argc, char** argv) {
     // Initialize the Goat
     Data& goatData = Data::get_instance();
@@ -89,5 +112,61 @@ int main(int argc, char** argv) {
     bmp3_status testbmp2Status = {{3,4,5},{6,7,8},{0,1,2},8};
     goatData.write(bmp2StatusReg, &testbmp2Status);
     dump = goatData.get();
-    assert(areEqualBmp3_status(dump.stat.bmp_status, testbmp2Status));
+    assert(areEqualBmp3_status(dump.stat.bmp_aux_status, testbmp2Status));
+
+    // Test NAV_SENSOR_ADXL1_DATA
+    Data::GoatReg adxl1DataReg = Data::GoatReg::NAV_SENSOR_ADXL1_DATA;
+    adxl375_data testadxl1Data = {1.5, 2.6, 3.7};
+    goatData.write(adxl1DataReg, &testadxl1Data);
+    dump = goatData.get();
+    assert(areEqualAdxl1375_data(dump.sens.adxl, testadxl1Data));
+
+    // Test NAV_SENSOR_ADXL2_DATA
+    Data::GoatReg adxl2DataReg = Data::GoatReg::NAV_SENSOR_ADXL2_DATA;
+    adxl375_data testadxl2Data = {3.7, 1.5, 2.6};
+    goatData.write(adxl2DataReg, &testadxl2Data);
+    dump = goatData.get();
+    assert(areEqualAdxl1375_data(dump.sens.adxl_aux, testadxl2Data));
+
+    // Test NAV_SENSOR_BMI1_ACCEL_DATA
+    Data::GoatReg bmi1AccelDataReg = Data::GoatReg::NAV_SENSOR_BMI1_ACCEL_DATA;
+    bmi08_sensor_data_f testBmi1AccelData = {3.7, 1.5, 2.6};
+    goatData.write(bmi1AccelDataReg, &testBmi1AccelData);
+    dump = goatData.get();
+    assert(areEqualbmi08_sensor_data_f(dump.sens.bmi_accel, testBmi1AccelData));
+
+    // Test NAV_SENSOR_BMI1_GYRO_DATA
+    Data::GoatReg bmi1GyroDataReg = Data::GoatReg::NAV_SENSOR_BMI1_GYRO_DATA;
+    bmi08_sensor_data_f testBmi1GyroData = {2.6, 3.7, 1.5};
+    goatData.write(bmi1GyroDataReg, &testBmi1GyroData);
+    dump = goatData.get();
+    assert(areEqualbmi08_sensor_data_f(dump.sens.bmi_gyro, testBmi1GyroData));
+
+    // Test NAV_SENSOR_BMI2_ACCEL_DATA
+    Data::GoatReg bmi2AccelDataReg = Data::GoatReg::NAV_SENSOR_BMI2_ACCEL_DATA;
+    bmi08_sensor_data_f testBmi2AccelData = {2.6, 3.7, 1.5};
+    goatData.write(bmi2AccelDataReg, &testBmi2AccelData);
+    dump = goatData.get();
+    assert(areEqualbmi08_sensor_data_f(dump.sens.bmi_aux_accel, testBmi2AccelData));
+
+    // Test NAV_SENSOR_BMI2_GYRO_DATA
+    Data::GoatReg bmi2GyroDataReg = Data::GoatReg::NAV_SENSOR_BMI2_GYRO_DATA;
+    bmi08_sensor_data_f testBmi2GyroData = {2.6, 3.7, 1.5};
+    goatData.write(bmi2GyroDataReg, &testBmi2GyroData);
+    dump = goatData.get();
+    assert(areEqualbmi08_sensor_data_f(dump.sens.bmi_aux_gyro, testBmi2GyroData));
+
+    // Test NAV_SENSOR_BMP1_DATA
+    Data::GoatReg bmp1DataReg = Data::GoatReg::NAV_SENSOR_BMP1_DATA;
+    bmp3_data testBmp1Data = {1.2, 2.3};
+    goatData.write(bmp1DataReg, &testBmp1Data);
+    dump = goatData.get();
+    assert(areEqualbmp3_data(dump.sens.bmp, testBmp1Data));
+
+    // Test NAV_SENSOR_BMP2_DATA
+    Data::GoatReg bmp2DataReg = Data::GoatReg::NAV_SENSOR_BMP2_DATA;
+    bmp3_data testBmp2Data = {2.1, 3.2};
+    goatData.write(bmp2DataReg, &testBmp2Data);
+    dump = goatData.get();
+    assert(areEqualbmp3_data(dump.sens.bmp_aux, testBmp2Data));
 }
