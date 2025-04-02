9a0f1246ae83caa70849da81fb565811d522d13b
bouaissim <menzo.bouaissi@epfl.ch>
2025-04-02 16:46:32 +0200

    simplify the test

--- tests/data_test.cpp
+++ tests/data_test.cpp
@@ -18,45 +18,14 @@ void test_simulation_mode()
     assert(dump.stat.bmi_gyro_status == 1);
     assert(dump.stat.bmp_status == 1);
     assert(dump.stat.bmp_aux_status == 1);
-    assert(dump.stat.adxl1_x == 0.0);
-    assert(dump.stat.adxl1_y == 0.0);
-    assert(dump.stat.adxl1_z == 0.0);
-    assert(dump.stat.adxl2_x == 0.0);
-    assert(dump.stat.adxl2_y == 0.0);
-    assert(dump.stat.adxl2_z == 0.0);
-    assert(dump.stat.bmi1_accel_x == 0.0);
-    assert(dump.stat.bmi1_accel_y == 0.0);
-    assert(dump.stat.bmi1_accel_z == 0.0);
-    assert(dump.stat.bmi1_gyro_x == 0.0);
-    assert(dump.stat.bmi1_gyro_y == 0.0);
-    assert(dump.stat.bmi1_gyro_z == 0.0);
-    assert(dump.stat.bmi2_accel_x == 0.0);
-    assert(dump.stat.bmi2_accel_y == 0.0);
-    assert(dump.stat.bmi2_accel_z == 0.0);
-    assert(dump.stat.bmi2_gyro_x == 0.0);
-    assert(dump.stat.bmi2_gyro_y == 0.0);
-    assert(dump.stat.bmi2_gyro_z == 0.0);
-    assert(dump.stat.bmp1_pressure == 0.0);
-    assert(dump.stat.bmp1_temperature == 0.0);
-    assert(dump.stat.bmp2_pressure == 0.0);
-    assert(dump.stat.bmp2_temperature == 0.0);
+
+
+    // Check if the data is being written correctly
+    assert(dump.sens.adxl.x == 0.0);
+    assert(dump.sens.adxl.y == 0.0);
+    assert(dump.sens.adxl.z == 0.0);
 
     std::cout << "Simulation mode test passed!\n";
-}
-
-void test_sensor_status()
-{
-    std::cout << "Testing sensor status updates...\n";
-
-    Sensors sensors;
-    sensors.update_status();
-    DataDump dump = Data::get_instance().get();
-    assert(dump.stat.adxl_status == 1);
-    assert(dump.stat.adxl_aux_status == 1);
-    assert(dump.stat.bmi_accel_status == 1);
-    assert(dump.stat.bmi_gyro_status == 1);
-    //assert(dump.stat.bmp_status == 1);
-    std::cout << "Sensor status test passed!\n";
 }
 
 int main(int argc, char **argv)
