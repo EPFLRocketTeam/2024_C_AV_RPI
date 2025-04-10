7bbc722ae8d22724914d864aa8fabad3180f0bf8
bouaissim <menzo.bouaissi@epfl.ch>
2025-04-10 17:39:20 +0200

    dep

--- src/sensor_drivers/MockAdxl375.h
+++ src/sensor_drivers/MockAdxl375.h
@@ -1,27 +1,28 @@
 #pragma once
 
-#include "adxl375.h"
+#include "sensor_drivers/adxl375.h"
+
 class MockAdxl375 : public Adxl375
 {
 public:
     MockAdxl375(uint8_t addr = 0) : Adxl375(addr) {}
 
-    adxl375_data get_data() override
+    virtual adxl375_data get_data() override
     {
         return Adxl375::get_data();
     }
 
-    uint8_t get_status() override
+    virtual uint8_t get_status() override
     {
         return Adxl375::get_status();
     }
 
-    adxl375_data add_noise_to_data(adxl375_data input, float stddev) override
+    virtual adxl375_data add_noise_to_data(adxl375_data input, float stddev) override
     {
         return Adxl375::add_noise_to_data(input, stddev);
     }
 
-    void calibrate() override
+    virtual void calibrate() override
     {
         Adxl375::calibrate();
     }
