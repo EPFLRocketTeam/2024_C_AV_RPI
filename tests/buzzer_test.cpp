fcb9e57b8c82492639f91a89481c7f357857ad8a
marinPh <marin.philippe@epfl.ch>
2025-07-17 20:11:09 +0200

    small changes to sensors tests

--- tests/buzzer_test.cpp
+++ tests/buzzer_test.cpp
@@ -6,6 +6,9 @@
 int main() {
     gpioInitialise();
 
+    gpioWrite(LED_LORA_RX, 1);
+    gpioWrite(LED_LORA_TX, 1);
+
     gpioWrite(BUZZER, 1);
     usleep(100e3);
     gpioWrite(BUZZER, 0);
@@ -17,6 +20,10 @@ int main() {
     gpioWrite(BUZZER, 1);
     usleep(100e3);
     gpioWrite(BUZZER, 0);
+
+    usleep(5e6);
+    gpioWrite(LED_LORA_RX, 0);
+    gpioWrite(LED_LORA_TX, 0);
 
     gpioTerminate();
 }
