fba2ebf7de310fccb1ff30d1c99a9ee43f0416f7
clacassa <cyprien.lacassagne@epfl.ch>
2025-08-30 12:04:40 +0200

    Added neopixel library for RGB status LED

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* PINOUTS / CONFIGS */
+/* GPIOS */
 #define LORA_UPLINK_CS      8
 #define LORA_UPLINK_RST     24
 #define LORA_UPLINK_DI0     5
@@ -10,17 +10,19 @@
 #define LORA_DOWNLINK_RST   25
 #define LORA_DOWNLINK_DI0   6
 
-#define INA228_LPB_SHUNT    65.2e-3
-#define INA228_LPB_MAX_CUR  2.5
+#define BUZZER              4
 
-#define INA228_HPB_SHUNT    27e-3
-#define INA228_HPB_MAX_CUR  6
+#define LED_STATUS          13
+#define LED_LORA_RX         17
+#define LED_LORA_TX         18
 
-#define BUZZER 4
+/* INA228 CONFIGS */
+#define INA228_LPB_SHUNT   65.2e-3
+#define INA228_LPB_MAX_CUR 2.5
 
-#define LED_STATUS  27
-#define LED_LORA_RX 17
-#define LED_LORA_TX 18
+#define INA228_HPB_SHUNT   27e-3
+#define INA228_HPB_MAX_CUR 6
+
 
 /* TIMINGS */
 #define MAIN_LOOP_MAX_FREQUENCY  100
